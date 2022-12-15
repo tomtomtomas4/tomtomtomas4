b0935f0eb07ed409dafccf2dbb9f2c70b918d37d
tomtomtomas4 <tpettitt95@gmail.com>
2022-12-15 23:46:51 +0000

    more

--- COL/TCL_files/script.js
+++ COL/TCL_files/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const heating = "If you can’t afford to put the heating on, why not simply "
 
-    const heatingOpts = ["start a bonfire outside your local flat roof pub?", "fire up the wood burning stove? You have got one, right?", "turn off the heating at whichever home you're not currently at?", "use your BBQ as a firepit in the front room? Crack a door a jar, obviously.", "go somewhere warmer?", "get a reasonably cheap BA flight to the middle east for a few weeks?", "dig around for an old unexploded bomb from the second world war? Toasty."]
+    const heatingOpts = ["do some volunteering at your local crematorium?", "rent out one of your rooms on AirBnB & charge them for the heating?", "start a bonfire outside your local flat roof pub?", "fire up the wood burning stove? You have got one, right?", "turn off the heating at whichever home you're not currently at?", "use your BBQ as a firepit in the front room? Crack a door a jar, obviously.", "go somewhere warmer?", "get a reasonably cheap BA flight to the middle east for a few weeks?", "dig around for an old unexploded bomb from the second world war?"]
 
     const medicine = "You can save money on fancy medicines by "
 
@@ -18,13 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const outings = "Avoid getting "
 
-    const outingsOpts = ["takeout", "Just Eat", "Deliveroo", "Dominos", "Papa Johns", "Burger King", "KFC", "Pizza Hut", "Mcdonalds", "Chinese takeout", "Indian", "Fish & chips", "Kebabs", "M&S", "Tesco Finest", "your local farm shop food"]
+    const outingsOpts = ["takeout", "Just Eat", "Deliveroo", "Dominoes", "Papa Johns", "Burger King", "KFC", "Pizza Hut", "Mcdonalds", "Chinese takeout", "Indian", "Fish & chips", "Kebabs", "M&S", "Tesco Finest"]
 
     const goings = [" in, & you could probably stick the central heating on for a little while.", ". Instead, get yourself a Nigella cookbook.", ". Plan your weekly shop & get it delivered from Ocado or Waitrose.", " in. Waitrose has a budget range."]
 
     const help = ["Feel... Helped, yet?", "Yo Dawg, I heard you need help", "SNP COL CRISIS", "CRISIS HELP", "More Advice Generated"]
 
-    const randomOpts = ["Just get a better job - there's plenty of jobs that surely must pay better than your current one, so the simplest advice has got to be to just get one of those ones.", "If you get a second job, and save really hard, eventually you will have enough to pay for your funeral. You may then die.", "Why weren't you born in an earlier decade. It's your fault you were born when you were.", "Get your parents to loan you a small amount to cover these “costs” or whatever, can’t be more than a few thousand, can it? You can pay them back after you cancel those subscriptions.", "Move into one of your parents properties for a while until you can get your spending under control.", "YOU'RE NEVER TOO OLD TO BE A STRIVER! A month picking fruit in one of multi-millionaire tory, Farmer Giles fields - and who knows, you may even have strived enough to pay off a week's gas bill.", "Have you tried using less washing up liquid? Avoid rinsing the dishes, just reuse them."]
+    const randomOpts = ["Just get a better job - there's plenty of jobs that surely must pay better than your current one, so the simplest advice has got to be to just get one of those ones.", "If you get a second job, and save really hard, eventually you will have enough to pay for your funeral. You may then die.", "Why weren't you born in an earlier decade. It's your fault you were born when you were.", "Get your parents to loan you a small amount to cover these “costs” or whatever, can’t be more than a few thousand, can it? You can pay them back after you cancel those subscriptions.", "Move into one of your parents properties for a while until you can get your spending under control.", "YOU'RE NEVER TOO OLD TO BE A STRIVER! A month picking fruit in one of multi-millionaire tory, Farmer Giles fields - and who knows, you may even have strived enough to pay off a week's gas bill."]
     
     let count = 0
 
