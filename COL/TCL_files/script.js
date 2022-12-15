f879b5486b10e0a16e91455498fb3ad1e1529904
tomtomtomas4 <tpettitt95@gmail.com>
2022-12-15 21:57:00 +0000

    Update script.js

--- COL/TCL_files/script.js
+++ COL/TCL_files/script.js
@@ -18,13 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const outings = "Avoid getting "
 
-    const outingsOpts = ["takeout", "Just Eat", "Deliveroo", "Dominoes", "Papa Johns", "Burger King", "KFC", "Pizza Hut", "Mcdonalds", "Chinese takeout", "Indian", "Fish & chips", "Kebabs", "M&S", "Tesco Finest"]
+    const outingsOpts = ["takeout", "Just Eat", "Deliveroo", "Dominoes", "Papa Johns", "Burger King", "KFC", "Pizza Hut", "Mcdonalds", "Chinese takeout", "Indian", "Fish & chips", "Kebabs", "M&S", "Tesco Finest", "your local farm shop food"]
 
     const goings = [" in, & you could probably stick the central heating on for a little while.", ". Instead, get yourself a Nigella cookbook.", ". Plan your weekly shop & get it delivered from Ocado or Waitrose.", " in. Waitrose has a budget range."]
 
     const help = ["Feel... Helped, yet?", "Yo Dawg, I heard you need help", "SNP COL CRISIS", "CRISIS HELP", "More Advice Generated"]
 
-    const randomOpts = ["Just get a better job - there's plenty of jobs that surely must pay better than your current one, so the simplest advice has got to be to just get one of those ones.", "If you get a second job, and save really hard, eventually you will have enough to pay for your funeral. You may then die.", "Why weren't you born in an earlier decade. It's your fault you were born when you were.", "Get your parents to loan you a small amount to cover these “costs” or whatever, can’t be more than a few thousand, can it? You can pay them back after you cancel those subscriptions.", "Move into one of your parents properties for a while until you can get your spending under control.", "YOU'RE NEVER TOO OLD TO BE A STRIVER! A month picking fruit in one of multi-millionaire tory, Farmer Giles fields - and who knows, you may even have strived enough to pay off a week's gas bill."]
+    const randomOpts = ["Just get a better job - there's plenty of jobs that surely must pay better than your current one, so the simplest advice has got to be to just get one of those ones.", "If you get a second job, and save really hard, eventually you will have enough to pay for your funeral. You may then die.", "Why weren't you born in an earlier decade. It's your fault you were born when you were.", "Get your parents to loan you a small amount to cover these “costs” or whatever, can’t be more than a few thousand, can it? You can pay them back after you cancel those subscriptions.", "Move into one of your parents properties for a while until you can get your spending under control.", "YOU'RE NEVER TOO OLD TO BE A STRIVER! A month picking fruit in one of multi-millionaire tory, Farmer Giles fields - and who knows, you may even have strived enough to pay off a week's gas bill.", "Have you tried using less washing up liquid? Avoid rinsing the dishes, just reuse them."]
     
     let count = 0
 
